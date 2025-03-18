63326a5440671a15090bc4959536540c18419ef5
Kaka556Kevin <kevinpampan8@gmail.com>
2025-03-18 17:39:32 +0700

    first commit

---
+++ app/Models/User.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel; // Tambahkan ini
+
+class User extends Authenticatable implements FilamentUser
+{
+    use HasFactory, Notifiable;
+
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'role', // Pastikan field role ada di database
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'email_verified_at' => 'datetime',
+            'password' => 'hashed',
+        ];
+    }
+
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return $this->role === 'admin'; // Pastikan role ada dalam database
+    }
+}
