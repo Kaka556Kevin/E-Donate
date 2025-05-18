e07ca418c30e9d4d888e9adee446f9f8d8c35e2e
Ahmad Yassin <ahmadyassin4521@gmail.com>
2025-05-18 10:56:08 +0700

    Seeder New

--- database/seeders/UangDonasiSeeder.php
+++ database/seeders/UangDonasiSeeder.php
@@ -7,63 +7,41 @@ use App\Models\UangDonasi;
 
 class UangDonasiSeeder extends Seeder
 {
+    /**
+     * Run the database seeds.
+     */
     public function run(): void
     {
-        $data = [
-            [
-                'nama_donasi' => 'Donasi Pendidikan Anak',
-                'uang_masuk' => 1000000,
-                'uang_keluar' => 250000,
-            ],
-            [
-                'nama_donasi' => 'Donasi Kesehatan Masyarakat',
-                'uang_masuk' => 1500000,
-                'uang_keluar' => 500000,
-            ],
-            [
-                'nama_donasi' => 'Donasi Bencana Alam',
-                'uang_masuk' => 2000000,
-                'uang_keluar' => 600000,
-            ],
-            [
-                'nama_donasi' => 'Donasi Sosial Yatim Piatu',
-                'uang_masuk' => 1200000,
-                'uang_keluar' => 300000,
-            ],
-            [
-                'nama_donasi' => 'Donasi Rumah Ibadah',
-                'uang_masuk' => 1800000,
-                'uang_keluar' => 400000,
-            ],
-            [
-                'nama_donasi' => 'Donasi Air Bersih',
-                'uang_masuk' => 1300000,
-                'uang_keluar' => 450000,
-            ],
-            [
-                'nama_donasi' => 'Donasi Lingkungan Hijau',
-                'uang_masuk' => 1100000,
-                'uang_keluar' => 200000,
-            ],
-            [
-                'nama_donasi' => 'Donasi Operasi Medis',
-                'uang_masuk' => 2500000,
-                'uang_keluar' => 750000,
-            ],
-            [
-                'nama_donasi' => 'Donasi Pendidikan Luar Negeri',
-                'uang_masuk' => 3000000,
-                'uang_keluar' => 1000000,
-            ],
-            [
-                'nama_donasi' => 'Donasi Panti Jompo',
-                'uang_masuk' => 1400000,
-                'uang_keluar' => 350000,
-            ],
+        $perusahaan = [
+            'Astra International',
+            'Bank Mandiri',
+            'Pertamina',
+            'Telkom Indonesia',
+            'Unilever Indonesia',
+            'Sinar Mas',
+            'Indofood Sukses Makmur',
+            'Wahana Mitra Perkasa',
+            'Jasindo',
+            'Djarum Foundation',
+            'Sampoerna Foundation',
+            'Salim Group',
+            'Panin Bank',
+            'XL Axiata',
+            'Lippo Group'
         ];
 
-        foreach ($data as $donasi) {
-            UangDonasi::create($donasi);
+        foreach ($perusahaan as $index => $pt) {
+            $uangMasuk = rand(50000, 5000000);
+            $uangKeluar = rand(0, $uangMasuk);
+            $saldo = $uangMasuk - $uangKeluar;
+
+            UangDonasi::create([
+                'nama_donasi' => "PT. $pt",
+                'uang_masuk' => $uangMasuk,
+                'uang_keluar' => $uangKeluar,
+                'saldo' => $saldo,
+                'kelola_donasi_id' => 1,
+            ]);
         }
     }
 }
